8069a88e3eaab4d94d4eca42085fe096c6350e80
Brian Murphy <bmurphy1@mac.com>
2014-03-25 14:29:39 -0500

    Write build_grid method

--- ocr.rb
+++ ocr.rb
@@ -1,3 +1,27 @@
 
+# Pseudocode
+
+
 class Ocr
+  def scan_for_account_numbers(file)
+    grid = build_grid(File.open(file, "r"))
+
+  end
+
+  private
+
+  def build_grid(file)
+    grid = []
+    file.each do |line|
+      line_array = []
+      line.chomp
+      until line.empty?
+        line_array << line.slice!(0,3)
+      end
+      grid << line_array
+    end
+    grid
+  end
+
+  
 end
