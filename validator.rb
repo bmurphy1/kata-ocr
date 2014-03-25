52cf4eeac1577091e3cfb1e720d78bae2f9575b8
Brian Murphy <bmurphy1@mac.com>
2014-03-25 16:30:12 -0500

    Write validate_account_number and split_account_number methods

--- validator.rb
+++ validator.rb
@@ -1,16 +1,34 @@
 
 # Pseudocode
 #
+# INPUT: account number (9-digit integer)
+# OUTPUT: true if account is valid, false otherwise
+# Example:
 # account number:  3  4  5  8  8  2  8  6  5
 # position names:  d9 d8 d7 d6 d5 d4 d3 d2 d1
 #
 # checksum calculation:
 # (d1+2*d2+3*d3 +..+9*d9) mod 11 = 0
 #
-# 
+# 1. split account number into array of single digits
+# 2. map array with index, each digit *
+# 3. multiply reduce array
+# 4. return true if reduced array mod 11 == 0
+#     false otherwise
 
 class Validator
-  def validate_account_number(number)
-
+  def validate_account_number(acc_num)
+    acc_array = split_account_number(acc_num)
+    acc_array.reverse!
+    acc_array.map!.with_index {|d, i| d * (i+1)}
+    acc_array.reduce(:+) % 11 == 0
   end
+
+  private
+
+  def split_account_number(acc_num)
+    acc_num.to_s.split("").map {|d| d.to_i}
+  end
+
+
 end
